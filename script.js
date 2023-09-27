dbd655deda6d3e7fb9362e8be125a961a7d63c6e
Owen <ocotten24@basecampcodingacademy.org>
2023-09-27 08:48:29 -0500

    first commit

--- script.js
+++ script.js
@@ -16,12 +16,14 @@ function createListElement() {
 	// implement makes text from input field the li text
 	// implement adds li to ul
 	// implement Reset text input field
+	var li = document.createElement(`Li`);
+	li.appendChild(document.createTextNode(input.value))
 
 
 	//START STRIKETHROUGH
 	// because it's in the function, it only adds it for new items
 	function crossOut() {
-		//implement me
+		event.target.classList.toggle("done");
 	}
 
 	li.addEventListener("click",crossOut);
@@ -29,15 +31,22 @@ function createListElement() {
 
 
 	// START ADD DELETE BUTTON
-
-		//Use the implemented function from below here
+	var deleteButton = document.createElement("button");
+	deleteButton.appendChild(document.createTextNode("X"));
+	deleteButton.addEventListener("click", deleteListItem);
+  
+	li.appendChild(deleteButton);
+  
+	ul.appendChild(li);
+  
+	input.value = "";
 	
 	// END ADD DELETE BUTTON
 
 
 	//ADD CLASS DELETE (DISPLAY: NONE)
 	function deleteListItem(){
-		//Implement me
+		event.target.parentElement.remove();
 	}
 	//END ADD CLASS DELETE
 }
@@ -46,7 +55,7 @@ function createListElement() {
 function addListAfterClick(){
 	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
 		createListElement();
-	}
+	}	
 }
 
 function addListAfterKeypress(event) {
